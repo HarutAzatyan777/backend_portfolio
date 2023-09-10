82744a426b5f90dcc9be1f84f11aa19216d041aa
HarutAzatyan777 <harutazatyan45@gmail.com>
2023-09-10 23:17:37 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import nodemailer from 'nodemailer';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.GMAIL_USER, // Your Gmail email address
+    pass:  process.env.GMAIL_PASSWORD, // Your Application-specific password
+  },
+});
+
+app.post('/api/send-email', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: 'harutazatyan45@gmail.com', // Sender's Gmail email address
+    to: 'harutazatyan45@gmail.com', // Recipient's email address
+    subject: 'Contact Form Submission',
+    text: `
+      Name: ${name}
+      Email: ${email}
+      Message: ${message}
+    `,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+
+    console.log('Email sent successfully.');
+
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
